5bd44ff1651e3b77faacb965bdc487f970fa689c
Craig Bell <craigdanielbell@gmail.com>
2017-01-17 15:08:19 +0000

    Created partials for splitting up the sections.

---
+++ includes/modules.php
@@ -0,0 +1,93 @@
+<?php
+?>
+
+					<ul data-role="listview" data-inset="true">
+						<li><a href="#itcarlow_popup" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-transition="pop">
+							<img src="images/institutes/itcarlow_logo.png">
+							<h2>BA (Hons) in Art</h2>
+							<p>Institute of Technology Carlow (2005 - 2009)</p>
+							</a>
+						</li>
+						<li><a href="#ucd_popup" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-transition="pop">
+							<img id="ucd_logo" src="images/institutes/ucd_logo.png">
+							<h2>MSc in Comp Science</h2>
+							<p>University College Dublin (2012 - 2013)</p>
+							</a>
+						</li>
+						<li><a href="#dit_popup" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-transition="pop">
+							<img src="images/institutes/dit_logo.png">
+							<h2>BSc (Ord) Digital Technology and Design</h2>
+							<p>Dublin Institure of Technology, WebElevate 4.0 (2015 - 2016)</p>
+							</a>
+						</li>
+					</ul>
+					
+
+					<div data-role="popup" id="itcarlow_popup">
+						<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>
+						<div data-role="header">
+							<h1>Modules:</h1>
+						 </div>
+						<div data-role="main" class="ui-content">
+							<ul data-role="listview" data-filter="true" data-filter-placeholder="Search modules..." data-inset="true">
+								<li>Art History</li>
+								<li>Audio/Video Editing</li>
+								<li>Cultural Studies</li>
+								<li>Professional Practices</li>
+								<li>Web Design</li>
+								<li><a href="https://www.itcarlow.ie/courses/type/undergraduate-cao-courses/wexford-courses/cw057.htm">More details[...]</a></li>
+							</ul>
+						</div>
+						<div data-role="footer">
+							<h1>Grade: 2.2</h1>
+						</div>
+					</div>
+										
+					<div data-role="popup" id="ucd_popup">
+						<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>
+						<div data-role="header">
+							<h1>Modules:</h1>
+						 </div>
+						<div data-role="main" class="ui-content">
+							<ul data-role="listview" data-filter="true" data-filter-placeholder="Search modules..." data-inset="true">
+								<li>Android Mobile Application Development Project</li>
+								<li>Exploring Programming in Ruby</li>
+								<li>Internet Technologies & Multimedia (IPA)</li>
+								<li>Introduction to Databases & Information Systems</li>
+								<li>Foundations of Hardware & Software (IPA)</li>
+								<li>Professional Java Programming Part 1</li>
+								<li>Programming I & II (C Programming)</li>
+								<li>Programming III (C++)</li>
+								<li>Software Engineering Project</li>
+								<li>SQL Programming</li>
+								<li>Systems Management (IPA)</li>
+								<li>Virtualisation and Cloud Computing (IPA)</li>
+								<li><a href="https://sisweb.ucd.ie/usis/!W_HU_MENU.P_PUBLISH?p_tag=PROG&MAJR=T195">More details[...]</a></li>
+							</ul>
+						</div>
+						<div data-role="footer">
+							<h1>Grade: 2.1</h1>
+						</div>
+					</div>
+					
+					<div data-role="popup" id="dit_popup">
+						<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>
+						<div data-role="header">
+							<h1>Modules:</h1>
+						 </div>
+						<div data-role="main" class="ui-content">
+							<ul data-role="listview" data-filter="true" data-filter-placeholder="Search modules..." data-inset="true">
+								<li>Digital Industries & Career Development</li>
+								<li>Entrepreneurship Creativity & Innovation</li>
+								<li>Industry Partner Project</li>
+								<li>Intro to Web Programming</li>
+								<li>Mobile Web Applications</li>
+								<li>Server-Side Web Development</li>
+								<li>UX & Concept Design</li>
+								<li><a href="http://www.webelevate.ie/">More details[...]</a></li>
+							</ul>
+						</div>
+						<div data-role="footer">
+							<h1>Grade: 2.1</h1>
+						</div>
+					</div>
---
+++ includes/skills.php
@@ -0,0 +1,21 @@
+<?php
+?>
+	
+	
+	<!-- Start of third page -->
+	<div data-role="page" id="skills">
+		<div data-role="header" data-theme="b">
+			<a href="#" data-rel="back" class="ui-btn-left ui-btn ui-icon-back ui-btn-icon-notext ui-shadow ui-corner-all" data-role="button" role="button">Back</a>
+			<a href="#mypanel" data-role="button" data-icon="bars" data-iconpos="notext" data-theme="b" data-inline="true" class="ui-link ui-btn-right ui-btn-b ui-icon-bars ui-btn-icon-notext ui-btn-inline ui-shadow ui-corner-all ui-first-child" role="button">My button</a>
+			<h1>Skills</h1>
+		</div><!-- /header -->
+
+		<div role="main" class="ui-content">
+			<p>I'm the third in the source order so I'm hidden when the page loads. I'm just shown if a link that references my id is beeing clicked.</p>
+		</div><!-- /content -->
+
+		<div data-role="footer" data-theme="b">
+			<p><a href="index.php">Back to Home</a></p>
+			<p class="reference"><a href="https://github.com/redbairn">© 2016 REDBAIRN</a></p>
+		</div><!-- /footer -->
+	</div><!-- /page -->
